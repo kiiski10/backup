2bc767e010e7a96439713b35652c14b0e96494b8
kiiski10 <pekka@pekka.pl>
2017-10-03 14:07:52 +0300

    Update organizer.sh
    mv -> cp

--- organizer.sh
+++ organizer.sh
@@ -4,7 +4,7 @@ backup_storage_dir="/home/backup/storage"
 
 #     Move the latest backup directory from daily to weekly storage dir
 latest=$(ls -t $backup_storage_dir/daily | head -1)
-mv "$backup_storage_dir/daily/$latest" $backup_storage_dir/weekly
+cp "$backup_storage_dir/daily/$latest" $backup_storage_dir/weekly
 
 #     Delete directories older than 30 days
 find "$backup_storage_dir/weekly" -mtime +30 -type d -delete  
