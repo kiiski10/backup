97d1dbed1fbf7b2d0b7e4dbe7804d0ecc8ce1737
kiiski10 <pekka@pekka.pl>
2017-10-22 13:16:26 +0300

    Update organizer.sh

--- organizer.sh
+++ organizer.sh
@@ -2,9 +2,9 @@
 timestamp=$(date +%d-%m-%Y_%H-%M-%S)
 backup_storage_dir="/home/backup/storage"
 
-#     Copy the latest backup directory from daily to weekly storage dir
+#     Move the latest backup directory from daily to weekly storage dir
 latest=$(ls -t $backup_storage_dir/daily | head -1)
-cp "$backup_storage_dir/daily/$latest" $backup_storage_dir/weekly
+mv "$backup_storage_dir/daily/$latest" $backup_storage_dir/weekly
 
 #     Delete directories older than 30 days
 find "$backup_storage_dir/weekly" -mtime +30 -type d -delete
